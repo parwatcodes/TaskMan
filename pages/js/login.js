662f7010d2b5af6d60de88202867f3db9fbf7a06
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-06-08 23:45:48 -0400

    clean-up logs

--- pages/js/login.js
+++ pages/js/login.js
@@ -1,15 +1,10 @@
 import { goto } from '../../helpers/utils.js';
 import { getAllUsers, persistUser, getAppData } from '../../helpers/api.js';
-import { isAuthenticated } from '../../helpers/validators.js'
 
  window.login = function(email, password) {
   let users = getAppData()?.['users'];
 
   let user = users.find(user => user.email === email);
-
-  console.log('uu', user)
-
-  isAuthenticated();
 
   if (user && user.password === password) {
     persistUser(email);
