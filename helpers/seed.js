28879d851ad756322d0be43a691f741cffedab2a
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-06-12 21:14:59 -0400

    Fix seed member issue

--- helpers/seed.js
+++ helpers/seed.js
@@ -1,7 +1,7 @@
 // Run when the app loads.
 import { getAppData } from './api.js';
 import { stringifyIt, parseIt } from './utils.js';
-import { APP_DATA_KEY, DEFAULT_ADMIN, ADMIN } from './constants.js';
+import { APP_DATA_KEY, DEFAULT_ADMIN, ADMIN, DEFAULT_MEMBER } from './constants.js';
 
 function seedUser() {
   let users = getAppData()?.users;
@@ -9,7 +9,7 @@ function seedUser() {
 
   if (!isAdminThere) {
     let data = {
-      users: [DEFAULT_ADMIN]
+      users: [DEFAULT_ADMIN, DEFAULT_MEMBER]
     };
 
     localStorage.setItem(APP_DATA_KEY, stringifyIt(data));
