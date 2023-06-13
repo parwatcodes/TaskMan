827b84b9a58a0905e4a20c486cf361e0b46a8f82
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-06-12 21:04:57 -0400

    Add few global constants to be used

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,21 +2,31 @@ export const ADMIN = 'admin';
 export const MEMBER = 'member';
 export const URL = 'http://localhost:5050'
 export const APP_DATA_KEY = 'taskMan-data';
+export const URI_LOGIN = '/pages/login.html';
+export const URI_DASHBOARD = '/pages/dashboard.html';
 export const DEFAULT_ADMIN = {
   "id": 1,
-  "name": "Dave Reese",
+  "name": "Default Admin",
   "role": ADMIN,
-  "email": "dave.reese@gmail.com",
-  "password": "Dave@123"
+  "email": "admin123@gmail.com",
+  "password": "Admin@123"
+};
+export const DEFAULT_MEMBER = {
+  "id": 2,
+  "name": "Default Member",
+  "role": MEMBER,
+  "email": "member123@gmail.com",
+  "password": "Member@123",
+  "ratePerHr": 45
 };
 export const LIST = {
   TODO: 'ToDo',
   IN_PROGRESS: 'In Progress',
   IN_REVIEW: 'In Review',
   DONE: 'Done'
-}
+};
 export const TASK_PRIORITY = {
   LOW: 'low',
   medium: 'medium',
   HIGH: 'high'
-}
+};
