fc334d96113c2ca75155125a428549c5d0604eb1
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-06-15 19:22:13 -0400

    Fetch members for task modal

--- pages/js/globals.js
+++ pages/js/globals.js
@@ -1,5 +1,5 @@
 import { login } from './login.js';
-import { isAdmin } from '../../helpers/api.js'
+import { isAdmin, getMembers } from '../../helpers/api.js'
 import { generatePassword } from './add-user.js';
 import { handleOnChangeForTask } from './task.js';
 import { goto, logout } from '../../helpers/utils.js';
@@ -9,6 +9,7 @@ window.goto = goto;
 window.login = login;
 window.logout = logout;
 window.isAdmin = isAdmin;
+window.getMembers = getMembers;
 window.isAuthenticated = isAuthenticated;
 window.generatePassword = generatePassword;
 window.handleOnChangeForTask = handleOnChangeForTask;
