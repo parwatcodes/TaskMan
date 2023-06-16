3f9b1c762637d1b34ea4efdb1479bc5832d75f15
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-06-16 19:56:05 -0400

    Update global.js imports and window objects/methods

--- pages/js/globals.js
+++ pages/js/globals.js
@@ -1,6 +1,7 @@
 import { login } from './login.js';
+import { addTask } from '../../helpers/api.js';
 import { generatePassword } from './add-user.js';
-import { handleOnChangeForTask } from './task.js';
+import { showToast, hideToast } from './toast.js'
 import { goto, logout } from '../../helpers/utils.js';
 import { isAuthenticated } from '../../helpers/validators.js';
 import { isAdmin, getMembers, groupTaskByListName } from '../../helpers/api.js'
@@ -8,9 +9,11 @@ import { isAdmin, getMembers, groupTaskByListName } from '../../helpers/api.js'
 window.goto = goto;
 window.login = login;
 window.logout = logout;
+window.addTask = addTask;
 window.isAdmin = isAdmin;
+window.showToast = showToast;
+window.hideToast = hideToast;
 window.getMembers = getMembers;
 window.isAuthenticated = isAuthenticated;
 window.generatePassword = generatePassword;
 window.groupTaskByListName = groupTaskByListName;
-window.handleOnChangeForTask = handleOnChangeForTask;
