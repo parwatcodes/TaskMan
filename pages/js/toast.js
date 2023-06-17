861f95a9fe557ce97382920b017c5353d1ea85bb
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-06-16 21:24:07 -0400

    Fix toast file

--- pages/js/toast.js
+++ pages/js/toast.js
@@ -2,6 +2,7 @@ const toast = document.getElementById('toast');
 
 export function showToast(msg) {
   if (!msg) return;
+
   toast.querySelector('.toast-body').innerHTML = msg;
   toast.classList.add('visible');
   document.getElementById('toast').style.display = 'block'
