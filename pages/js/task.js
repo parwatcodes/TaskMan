e172a1f0e5892033d5da2bc70dcb56a892fe1846
Parwat Kunwar <parwatkunwar08@gmail.com>
2023-06-16 21:24:40 -0400

    Nuke unwanted file

--- pages/js/task.js
+++ pages/js/task.js
@@ -1,7 +1,5 @@
-import { getTaskList } from '../../helpers/api.js';
-
-// onChange handler for task name and description.
-export function handleOnChangeForTask(event) {
-
-  // console.log('ixxx', event.target.id, event.target.value);
-}
+// {
+//   taskMan-Data: {
+//     tasks: []
+//   }
+// }
